44a869162f2b96a55018ad817fd3bb314eb8bd63
davinciadmin <davinciadmin@SEDAVINCIAPPT02.int.stadium.biz>
2020-07-08 07:31:54 +0200

    'commit'

--- loadLogAnalysis.bat
+++ loadLogAnalysis.bat
@@ -1,4 +1,5 @@
+cd C:\Users\davinciadmin\stadiumLogAnalysis
 sqlplus -s assort1800/gtuiASY1299@stadavp.int.stadium.biz:1521/STADAVP @loadcsv\load.sql
-git add .
+git add *
 git commit -m 'commit'
 git push
