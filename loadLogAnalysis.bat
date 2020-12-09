a7ea535f3c8ec7b8b3a24bf1599bcf623111796f
jasonzhuangdavinci <jason.zhuang@davinciretail.com>
2020-12-09 22:45:56 +0100

    'commit'

--- loadLogAnalysis.bat
+++ loadLogAnalysis.bat
@@ -1,6 +1,6 @@
 cd C:\Users\davinciadmin\stadiumLogAnalysis
 sqlplus -s assort1800/gtuiASY1299@stadavp.int.stadium.biz:1521/STADAVP @loadcsv\load.sql
-rem sqlplus -s assort1830/gtuiASY1299@stadavp.int.stadium.biz:1521/STADAVP @loadcsv\loadoutlet.sql
+sqlplus -s assort1830/xzytre8913@stadavp.int.stadium.biz:1521/STADAVP @loadcsv\loadoutlet.sql
 
 git config --global user.email "jason.zhuang@davinciretail.com"
 git config --global user.name "jasonzhuangdavinci"
